dd88db8a92fe6fab1b13047c6c30912bd79f1615
glidder <faceless.goblin@gmail.com>
2013-10-18 11:57:54 +0200

    First commit

---
+++ MC/P3/configuracion.java
@@ -0,0 +1,13 @@
+/**
+* @author Luis José Quintana Bolaño
+* @version 0.01
+**/
+
+public class configuracion
+{
+	public configuracion(){
+		
+	}
+	
+	private int indice=0;
+}
---
+++ MC/P3/interprete.java
@@ -0,0 +1,21 @@
+/**
+* @author Luis José Quintana Bolaño
+* @version 0.01
+**/
+
+import java.utils.*;
+import java.io.*;
+
+
+public class interprete
+{
+	public static void procesarArchivo(String ruta) {
+		File code = new File(ruta);
+		//http://www.javaprogrammingforums.com/file-input-output-tutorials/339-reading-file-line-line-using-scanner-class.html
+	}
+	public static void main(String[] args) {
+		programa = procesarArchivo((args.length==0)?"code.s":args[0]);
+			
+		
+	}
+}
