a5b164aec6f9af4a531bad4ed0cb2715d6117552
glidder <faceless.goblin@gmail.com>
2013-10-28 17:15:37 +0100

    little

--- MC/P3/interprete.java
+++ MC/P3/interprete.java
@@ -3,18 +3,41 @@
 * @version 0.01
 **/
 
-import java.utils.*;
+import java.util.*;
+import java.util.regex.*;
 import java.io.*;
 
 
 public class interprete
 {
-	public static void procesarArchivo(String ruta) {
-		File code = new File(ruta);
-		//http://www.javaprogrammingforums.com/file-input-output-tutorials/339-reading-file-line-line-using-scanner-class.html
+	public static programa procesarArchivo(String[] args) {
+		File file = new File(args[0]);
+		scanner 
+		programa p=new programa();
+		Pattern var = Pattern.compile("([XZ][0-9]*)|(Y)");
+		Pattern zero= Pattern.compile(" *("+var+") *[=] *0 *");
+		Pattern succ= Pattern.compile(" *("+var+")[+][+] *");
+		Pattern copy= Pattern.compile(" *("+var+") *[=] *("+var+") *");
+		Pattern loop= Pattern.compile(" *LOOP *("+var+") *");
+		Pattern lend= Pattern.compile(" *END *");
+ 
+        try {
+            Scanner scanner = new Scanner(file);
+            Matcher m;
+            while (scanner.hasNextLine()) {
+                m = zero.matcher(scanner.nextLine());
+                if(m.matches()) System.out.println("YAY");
+                
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Archivo no encontrado.");
+        }
+        return p;
 	}
+	
 	public static void main(String[] args) {
-		programa = procesarArchivo((args.length==0)?"code.s":args[0]);
+		programa p = procesarArchivo(args);
 			
 		
 	}
---
+++ MC/P3/programa.java
@@ -0,0 +1,13 @@
+/**
+* @author Luis José Quintana Bolaño
+* @version 0.01
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class programa
+{
+	public programa(){}
+	private  configuracion conf;
+}
