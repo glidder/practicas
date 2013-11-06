0dd1d77c52e9c2fe4dc396866774447ebda320cd
glidder <faceless.goblin@gmail.com>
2013-11-06 03:10:41 +0100

    TALF entrega 2, release candidate

---
+++ TALF/entrega2/ej.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+#include <cstdio>
+
+using namespace std;
+
+int line=1;
+map <string, string> tres;
+map <string, string> tint;
+map <string, string> tid;
+
+extern FILE* yyin;
+
+int main() {
+	yylex();
+	cout << "-----------------\nPalabras reservadas & lineas de aparicion\n"
+	     << "----------\n";
+	for (map<string,string>::iterator i = tres.begin();i != tres.end();i++){
+			cout << i->first << "=" << i->second << endl;}
+	cout << "-----------------\n"
+	     << "Fin \n"
+	     << "----------\n";
+} // fin de main() 
+/*string toString(const int& port) {
+    stringstream ss;
+    ss << port;
+    return ss.str();
+}
+{preser}	tres[string(yytext)]+=" "+line ;
+{centera}	tint[string(yytext)]+=" "+line ;
+{ident}		tid[string(yytext)]+=" "+line ;*/
+for int int for while else for
