fa6c6ec79f5dd24bbe60677154c24192a4b6fe17
glidder <faceless.goblin@gmail.com>
2014-05-29 16:20:55 +0200

    Just in case

---
+++ AC/RN/koh/som.m
@@ -0,0 +1,19 @@
+function W=som(entradas,W,vecindad,T,a0)
+	N=size(entradas, 2);
+	M=size(W,2);
+	for t=1:T
+		a=a0*(1-t/T);
+		for i=1:N
+			[~,g]=min(dist(W',entradas(:,i)));
+			for j=1:M
+				inc=a*(entradas(:,i)-W(:,g));
+				W(:,j)=	W(:,j)+(inc*((1/2)^vecindad(j,g)));
+			end
+		end
+		plot(entradas(1,:),entradas(2,:),'.');
+		hold on;
+		plotsom(W',vecindad);
+		pause(0.1);
+		%hold off;
+	end	
+end
