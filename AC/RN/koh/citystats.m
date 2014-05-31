c8562a18d97dc28010c36beaf047629d9b1a1914
glidder <luisjquintana@gmail.com>
2014-05-31 15:56:47 +0200

    Jsm

--- AC/RN/koh/citystats.m
+++ AC/RN/koh/citystats.m
@@ -1,29 +1,41 @@
 clear all;
 load cities.mat
+ratings=ratings';
 
-positions = hextop(6,6);
-
-
-w=gridtop(6,6);
+%Cojemos salud y economia
+ratings=ratings([3,9],:);
+%size(ratings)
+plo=0;
+for ta=[2,3,4,6]
+    plo=plo+1;
+w=gridtop(ta,1);
 vecindad=linkdist(w);
-
 W=som(ratings,w,vecindad,50,0.2);
 
-clase=zeros(1,length(datos));
-for i=1:length(datos);
-    [~,clase(i)]=min(dist(W', datos(:,i)));
+clase=zeros(1,length(ratings));
+for i=1:length(ratings);
+    [~,clase(i)]=min(dist(W', ratings(:,i)));
 end
 
-close all;
-colores=['ob'; 'or'; 'om'; 'og'; 'oy'];
-j=0;
-figure, plotsom(W',vecindad);hold on;
+%close all;
+%colores=['ob'; 'or'; 'om'; 'og'; 'oy'];
+%j=0;
+hold off;
+subplot(2,2,plo)
+plotsom(W',vecindad);hold on;
+        plotpat(ratings,clase,'.')
 for i=1:length(clase)
-	j=j+1;
-		plot(datos(1,find(clase==i)),datos(2,find(clase==i)),colores(j,:));
-	if(j==5)
-		j=0;
-	end
+	%j=j+1;
+	%	plot(ratings(1,find(clase==i)),ratings(2,find(clase==i)),colores(j,:));%hold on
+	%if(j==5)
+	%	j=0;
+    %end
+    %disp('Categori '); i
+    if (~isempty(names(find(clase==i),:)))
+        i
+        names(find(clase==i),:)
+    end
+end
 end
 
 %plotpat(puntos,clase,'.');
